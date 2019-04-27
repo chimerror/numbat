4ed6bbc9a60e185749f2cfd1bbace9bda524cb80
Jason Mitchell <foobardog@hotmail.com>
2019-04-26 19:31:47 -0700

    Added Stats from TCSG, Added ball that moves on input (for now)

--- Assets/numbat/core.clj
+++ Assets/numbat/core.clj
@@ -1,5 +1,25 @@
 (ns numbat.core
-  (:use arcadia.core))
+  (:use arcadia.core
+        arcadia.linear)
+  (:import [UnityEngine
+            Input Vector2 Transform]))
 
-(defn rotate [obj role-key]
-  (.. obj transform (Rotate 0 1 0)))
+(def ball-initial
+  {:position Vector2/zero})
+
+(defn move [obj offset]
+  (state+ obj :position (update (state obj :position) :position v2+ offset))
+  obj)
+
+(defn set-position [obj position]
+  (with-cmpt obj [tr Transform]
+    (set! (. tr position)
+          (v3 (.x position)
+              (.y position)
+              0))))
+
+(defn input-move [obj role-key]
+  (-> obj
+      (move (v2 (Input/GetAxis "Horizontal")
+                (Input/GetAxis "Vertical")))
+      (set-position (:position (state obj role-key)))))
