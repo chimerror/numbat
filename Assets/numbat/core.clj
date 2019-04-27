d28bc1957c91836ee13502c3c757bccdafb0ce8f
Jason Mitchell <foobardog@hotmail.com>
2019-04-27 13:55:38 -0700

    Adjusting ball to size 5, making player human sized, scaling input

--- Assets/numbat/core.clj
+++ Assets/numbat/core.clj
@@ -7,6 +7,12 @@
 (def ball-initial
   {:position Vector2/zero})
 
+(def player-initial
+  {:position (v2 -0.5 0) :has-ball false})
+
+(def input-scale
+  0.25)
+
 (defn move [obj offset]
   (state+ obj :position (update (state obj :position) :position v2+ offset))
   obj)
@@ -20,6 +26,14 @@
 
 (defn input-move [obj role-key]
   (-> obj
-      (move (v2 (Input/GetAxis "Horizontal")
-                (Input/GetAxis "Vertical")))
+      (move (v2 (* (Input/GetAxis "Horizontal") input-scale)
+                (* (Input/GetAxis "Vertical") input-scale)))
       (set-position (:position (state obj role-key)))))
+
+(defn trap-ball [obj role-key collision]
+  (if (= (.. collision gameObject name) "Ball")
+    (do
+      (state+ obj :position (assoc (state obj :position) :has-ball true))
+      (child+ obj (.. collision gameObject) true))
+    obj))
+
